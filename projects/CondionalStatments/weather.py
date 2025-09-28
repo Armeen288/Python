a0ef4827bf9f796b0f4e669d5c932cbbe5265e63
Armeen288 <letsplaytogether40@gmail.com>
2025-09-28 14:41:09 +0500

    final

---
+++ projects/CondionalStatments/weather.py
@@ -0,0 +1,8 @@
+Hot = 40
+cold = 25
+temp = int(input("Enter The Temperature: "))
+if temp >= Hot:
+    print("It's very Hot outside, you shouldnt wear warm clothes, maybe get some ice cream")
+
+if temp <=cold:
+    print("It's very Cold outside, you should wear warm clothes, maybe get some hot chocolate")
