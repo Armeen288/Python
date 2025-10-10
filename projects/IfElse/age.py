35e37723a83b8e3d76db20c64562f2e85a4a76fd
Armeen288 <letsplaytogether40@gmail.com>
2025-10-10 14:44:42 +0500

    final

---
+++ projects/IfElse/age.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age: "))
+
+if age >= 10:
+
+    if age <= 20:
+        print("Your age is between 10 and 20.")
+
+    else:
+        print("Your age is not between 10 and 20.")
+
+else:
+    print("Your age is not between 10 and 20.")
